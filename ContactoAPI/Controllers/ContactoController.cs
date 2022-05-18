6cd23f3d0000f815a1d20b8d1f68db3a74c6783a
Javier De Jesus <ismael.dt@hotmail.com>
2022-05-18 08:02:02 -0400

    Creando controlador

---
+++ ContactoAPI/Controllers/ContactoController.cs
@@ -0,0 +1,26 @@
+﻿using Mantenimiento.Models;
+using Mantenimientos.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactoController: ControllerBase
+    {
+        public ResponseModel<Contacto> GetContatos()
+        {
+            return ContactoLogic.GetContatos();
+        }        
+        
+        public ResponseModel<Contacto> DeleteContactoById([FromBody] int Id)
+        {
+            return ContactoLogic.DeleteContactoById(Id);
+        }        
+        
+        public ResponseModel<Contacto> GetContatos(Contacto model)
+        {
+            return ContactoLogic.InsertContacto(model);
+        }
+    }
+}
